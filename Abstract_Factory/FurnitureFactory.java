7cfdff0fd368d23367ba4c8e3968a632397d4831
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-02-11 09:55:10 +0700

    demo added

---
+++ Abstract_Factory/FurnitureFactory.java
@@ -0,0 +1,8 @@
+package DP.Abstract_Factory;
+
+public interface FurnitureFactory {
+    public void CreateChair();
+    public void CreateCoffeeTable();
+    public void CreateSofa();
+    public void CommercialBreak();
+}
